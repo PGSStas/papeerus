2e024bd720e5e498d4756ac5e50e9ad55bfc3648
Stas Katsuba <stas.katsuba@gmail.com>
2021-03-30 20:12:11 +0300

    Fix review and add correct client deletion

--- chat_cipher.py
+++ chat_cipher.py
@@ -5,9 +5,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 class ChatCipher:
-    def __init__(self, token, src, iv):
-        assert isinstance(token, bytes), "Invalid token"
-        assert isinstance(iv, bytes), "Invalid iv"
+    def __init__(self, token: bytes, src, iv: bytes):
         assert len(token) == 160, "Invalid token"
 
         self.token = token
@@ -15,28 +13,24 @@ class ChatCipher:
 
         self.cipher = Cipher(algorithms.AES(self.token), modes.CBC(iv))
 
-    def encrypt_message(self, message):
-        assert isinstance(message, bytes), "Invalid message"
-
+    def encrypt_message(self, message: bytes):
         padder = padding.PKCS7(128).padder()
         p_data = padder.update(message) + padder.finalize()
         encryptor = self.cipher.encryptor()
         e_data = encryptor.update(p_data) + encryptor.finalize()
         return e_data
 
-    def decrypt_message(self, message):
-        assert isinstance(message, bytes), "Invalid message"
-
+    def decrypt_message(self, message: bytes):
         decryptor = self.cipher.decryptor()
         de_data = decryptor.update(message) + decryptor.finalize()
         unpadder = padding.PKCS7(128).unpadder()
         up_data = unpadder.update(de_data) + unpadder.finalize()
         return up_data
 
-    def serialize(self, message):
+    def serialize(self, message: bytes):
         data = {self.encrypt_message(self.src), self.encrypt_message(message)}
         return pickle.dumps(data)
 
-    def deserialize(self, data):
+    def deserialize(self, data: bytes):
         p_data = pickle.loads(data)
         return {self.decrypt_message(p_data[0]), self.decrypt_message(p_data[1])}
--- client.py
+++ client.py
@@ -1,21 +1,27 @@
 import os
 import socket
 import threading
+import sys
 
 
 class Client:
+    # Temp id for sockets
+    _sid = 0
+    # List of sockets, ciphers and threads for receive
+    _ids = []
+    _peers = []
+    _ciphers = []
+    _threads = []
+
     def __init__(self):
         print("Started client")
 
+        # Starting listener
         self._socket_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket_listener.bind(("localhost", 0))
         self._socket_listener.listen()
         print(f"Listening on 127.0.0.1:{self._socket_listener.getsockname()[1]}")
         self._mutex = threading.Lock()
-
-        self._peers = []
-        self._ciphers = []
-        self._threads = []
 
         self._accept_thread = threading.Thread(target=self._accept_connection)
         self._accept_thread.start()
@@ -25,13 +31,17 @@ class Client:
             q = input()
             ls = q.split(' ')
             if ls[0] == "quit":
+                # Exit from program
                 os._exit(0)
             elif ls[0] == "ls":
+                # Print all connection and their IDs
                 for i in range(0, len(self._peers)):
                     print(f"#{i} {self._peers[i][1][0]}:{self._peers[i][1][1]}")
             elif ls[0] == "connect":
+                # Connect to new client
                 self._establish_connection(ls[1], ls[2])
             else:
+                # Send message to Client with specific ID
                 self._send(self._peers[int(ls[0])][0], q.split(' ', 1)[1])
 
     def _accept_connection(self):
@@ -39,36 +49,45 @@ class Client:
             connection = self._socket_listener.accept()
 
             self._mutex.acquire()
+            self._sid += 1
+            self._ids.append(self._sid)
             self._peers.append(connection)
             thread = threading.Thread(target=self._receive,
-                                      args=(connection[0],
+                                      args=(self._sid, connection[0],
                                             connection[1][0] + ":" + str(connection[1][1])))
             thread.start()
             self._threads.append(thread)
             self._mutex.release()
 
-    def _establish_connection(self, address, port):
+    def _establish_connection(self, address: str, port: str):
         socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_client.connect((address, int(port)))
 
         self._mutex.acquire()
+        self._sid += 1
+        self._ids.append(self._sid)
         self._peers.append((socket_client, (address, port)))
         thread = threading.Thread(target=self._receive,
-                                  args=(socket_client, address + ":" + port))
+                                  args=(self._sid,
+                                        socket_client, address + ":" + port))
         thread.start()
         self._threads.append(thread)
         self._mutex.release()
 
-    def _send(self, sock, message):
+    def _send(self, sock: socket.socket, message: str):
         data = message.encode()
         sock.send(data)
 
-    def _receive(self, sock, address):
+    def _receive(self, sid: int, sock: socket.socket, address: str):
         while True:
             msg = sock.recv(4096)
+            if msg == b"":
+                self._mutex.acquire()
+                index = self._ids.index(sid)
+
+                self._ids.pop(index)
+                self._peers.pop(index)
+                self._threads.pop(index)
+                self._mutex.release()
+                sys.exit(0)
             print(f"{address}: {msg.decode()}")
-
-
-if __name__ == '__main__':
-    client = Client()
-    client.input_cycle()
