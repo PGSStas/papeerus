370221639b1f6c89972c112bc72f1c023a966133
Stanislau Katsuba <50747082+PGSStas@users.noreply.github.com>
2021-03-31 13:58:26 +0300

    Merge pull request #1
    Добавлен скелет класса Client. Возможно одновременно переписываться между несколькими локально расположенными клиентами.

---
+++ chat_cipher.py
@@ -0,0 +1,36 @@
+import pickle
+
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+class ChatCipher:
+    def __init__(self, token: bytes, src, iv: bytes):
+        assert len(token) == 160, "Invalid token"
+
+        self.token = token
+        self.src = src
+
+        self.cipher = Cipher(algorithms.AES(self.token), modes.CBC(iv))
+
+    def encrypt_message(self, message: bytes):
+        padder = padding.PKCS7(128).padder()
+        p_data = padder.update(message) + padder.finalize()
+        encryptor = self.cipher.encryptor()
+        e_data = encryptor.update(p_data) + encryptor.finalize()
+        return e_data
+
+    def decrypt_message(self, message: bytes):
+        decryptor = self.cipher.decryptor()
+        de_data = decryptor.update(message) + decryptor.finalize()
+        unpadder = padding.PKCS7(128).unpadder()
+        up_data = unpadder.update(de_data) + unpadder.finalize()
+        return up_data
+
+    def serialize(self, message: bytes):
+        data = {self.encrypt_message(self.src), self.encrypt_message(message)}
+        return pickle.dumps(data)
+
+    def deserialize(self, data: bytes):
+        p_data = pickle.loads(data)
+        return {self.decrypt_message(p_data[0]), self.decrypt_message(p_data[1])}
