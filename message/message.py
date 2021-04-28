6816fb793af1922608a5af8de1ecac953403f3d6
Daniil Dengalev <p.rogrammer228@yandex.ru>
2021-04-28 21:36:43 +0300

    real fast video translate

--- message/message.py
+++ message/message.py
@@ -1,42 +1,31 @@
 class MessageSerializer:
     def serializeMessage(self, message, type):
         if type == 'text':
-            return type + message
+            ans = bytearray()
+            ans += b'0'
+            ans += message.encode()
+            return ans
         if type == "picture":
             data = open(file=message, mode="rb").read()
-            binary_str = ""
-            for i in data:
-                binary_str += str(i // 100)
-                binary_str += str((i // 10) % 10)
-                binary_str += str(i % 10)
-            # print((type+binary_str).__class__)
-            return (type + binary_str)
+            ans = bytearray()
+            ans += b'1'
+            ans += data
+            return ans
         if type == "video":
             data = open(file=message, mode="rb").read()
-            binary_str = ""
-            for i in data:
-                binary_str += str(i // 100)
-                binary_str += str((i // 10) % 10)
-                binary_str += str(i % 10)
-            # print((type+binary_str).__class__)
-            return (type + binary_str)
+            ans = bytearray()
+            ans += b'2'
+            ans += data
+            return ans
 
-    def deserializeMessage(self, message: str):
-        if len(message) > 4 and message[:4] == "text":
-            return ["text", message[4:]]
-        if len(message) > 7 and message[:7] == "picture":
-            arr = bytearray()
-            i = 7
-            while i < len(message):
-                arr.append(int(message[i]) * 100 + int(message[i + 1]) * 10 + int(message[i + 2]))
-                i += 3
+    def deserializeMessage(self, message: bytes):
+        if len(message) > 0 and message[0] == 48:
+            return ["text", message[1:].decode()]
+        if len(message) > 0 and message[0] == 49:
+            arr = message[1:]
             kek = open(file="brat.jpg", mode="wb").write(arr)
             return ["picture", "42"]
-        if len(message) > 5 and message[:5] == "video":
-            arr = bytearray()
-            i = 5
-            while i < len(message):
-                arr.append(int(message[i]) * 100 + int(message[i + 1]) * 10 + int(message[i + 2]))
-                i += 3
+        if len(message) > 0 and message[0] == 50:
+            arr = message[1:]
             kek = open(file="video.mp4", mode="wb").write(arr)
             return ["video", "42"]
